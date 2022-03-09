d3848f692856b1fb9aeff939eb4f74c20457f5c8
Yiğithan Yücedağ <yigithanyucedag@gmail.com>
2022-03-10 01:29:19 +0300

    number casting added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,24 @@
 import type { NextPage } from "next";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../app/store";
 import Table from "../components/Table";
+import { update } from "../features/price/priceSlice";
+import { addRow, deleteRow } from "../features/table/tableSlice";
 
 const Home: NextPage = () => {
+  const dispatch = useDispatch();
   const items = useSelector((state: RootState) => state.table);
+  const price = useSelector((state: RootState) => state.price);
+
+  const selectedItems = items.filter((item) => item.selected);
   const allDevices = items.length;
   const totalPowerConsumption = items.reduce(
     (acc, item) =>
       acc +
-      ((item.watt * (item.workingHours * item.weeklyUsage)) / 1000) *
-        item.amount *
+      ((Number(item.watt) *
+        (Number(item.workingHours) * Number(item.weeklyUsage))) /
+        1000) *
+        Number(item.amount) *
         4,
     0
   );
@@ -19,8 +27,10 @@ const Home: NextPage = () => {
   const mostPowerConsumingDevice = items.reduce(
     (acc, item) => {
       const powerConsumption =
-        ((item.watt * (item.workingHours * item.weeklyUsage)) / 1000) *
-        item.amount *
+        ((Number(item.watt) *
+          (Number(item.workingHours) * Number(item.weeklyUsage))) /
+          1000) *
+        Number(item.amount) *
         4;
       if (powerConsumption > acc.powerConsumption) {
         return {
@@ -36,6 +46,49 @@ const Home: NextPage = () => {
   return (
     <>
       <h1 className="text-3xl font-bold"></h1>
+      <div className="px-2 pb-4 pt-2 flex items-center flex-col sm:flex-row justify-center sm:justify-end gap-2 overflow-visible">
+        <div className="form-control w-full sm:max-w-xs mr-0 sm:mr-auto">
+          <label className="label">
+            <span className="label-text">Elektrik kWh bedeli (₺)</span>
+          </label>
+          <input
+            type="text"
+            value={price.value}
+            onChange={(event) => dispatch(update(event.target.value))}
+            placeholder="1.25"
+            className="input input-bordered w-full sm:max-w-xs input-sm"
+          />
+        </div>
+        <button
+          className="btn btn-error btn-block sm:w-auto btn-sm"
+          disabled={selectedItems.length === 0}
+          onClick={() => {
+            for (let item of selectedItems) {
+              dispatch(deleteRow(item));
+            }
+          }}
+        >
+          Sil
+        </button>
+        <button
+          onClick={() => {
+            dispatch(
+              addRow({
+                id: Math.floor(Math.random() * Date.now()),
+                selected: false,
+                deviceName: "",
+                watt: "" as any,
+                amount: "1",
+                workingHours: "1",
+                weeklyUsage: "1",
+              })
+            );
+          }}
+          className="btn btn-info btn-block sm:w-auto btn-sm"
+        >
+          Cihaz Ekle
+        </button>
+      </div>
       <div className="mt-5">
         <Table />
       </div>
@@ -49,14 +102,18 @@ const Home: NextPage = () => {
           <div className="stat">
             <div className="stat-title">Max. Enerji Tüketen Cihaz</div>
             <div className="stat-value">
-              {mostPowerConsumingDevice.deviceName}
+              {mostPowerConsumingDevice.deviceName
+                ? mostPowerConsumingDevice.deviceName.length > 10
+                  ? `${mostPowerConsumingDevice.deviceName.substring(0, 10)}…`
+                  : mostPowerConsumingDevice.deviceName
+                : "-"}
             </div>
           </div>
 
           <div className="stat">
             <div className="stat-title">Aylık Toplam Maliyet</div>
             <div className="stat-value">
-              {(totalPowerConsumption * 1.25).toFixed(2)}₺
+              {(totalPowerConsumption * Number(price.value)).toFixed(2)}₺
             </div>
           </div>
         </div>
